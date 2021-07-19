4062822345cff1a82bd776ab7f901252c3df2dd5
choiyh <choiyh44@gmail.com>
2021-07-19 16:34:43 +0900

    보완

--- src/main/java/kr/co/ensmart/frameworkdemo/app/controller/SampleController.java
+++ src/main/java/kr/co/ensmart/frameworkdemo/app/controller/SampleController.java
@@ -1,14 +1,23 @@
 package kr.co.ensmart.frameworkdemo.app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
 @RequestMapping("/sample")
+@Slf4j
 public class SampleController {
+	@Value("${app.name}")
+	private String appName;
+	
 	@GetMapping("/main")
 	String viewMain() {
+		log.info("test: {}", appName);
+		
 		return "sample/main";
 	}
 	
